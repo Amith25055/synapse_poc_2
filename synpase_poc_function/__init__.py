3496e62c254d605e9f276492cfe085594444dbf2
Amith25055 <104182312+Amith25055@users.noreply.github.com>
2023-02-15 17:28:38 +0530

    Update __init__.py

--- synpase_poc_function/__init__.py
+++ synpase_poc_function/__init__.py
@@ -10,32 +10,37 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     start_datetime=req.params.get('start_datetime')
     history_year=req.params.get('history_year')
 
+    try:
+        if end_point_name=='crimes_history':
+            url=url_crimes_hist='https://data.cityofchicago.org/resource/ijzp-q8t2.json?year='+str(history_year)
+            container_name='myfilesystem/Crimes/Crimes_History'
+            file_name='crimes_'+str(date.today())+'.json'
+            
+        elif end_point_name=='crimes_incr':
+            url='https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=updated_on >'+"'"+start_datetime+"'"
+            container_name='myfilesystem/Crimes/Crimes_Incremental'
+            file_name='crimes_'+str(history_year)+'.json'
 
-    if end_point_name=='crimes_history':
-        url=url_crimes_hist='https://data.cityofchicago.org/resource/ijzp-q8t2.json?year='+str(history_year)
-        container_name='myfilesystem/Crimes/Crimes_History'
-        file_name='crimes_'+str(date.today())+'.json'
-        
-    elif end_point_name=='crimes_incr':
-        url='https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=updated_on >'+"'"+start_datetime+"'"
-        container_name='myfilesystem/Crimes/Crimes_Incremental'
-        file_name='crimes_'+str(history_year)+'.json'
+        elif end_point_name=='arrests_incr':
+            url='https://data.cityofchicago.org/resource/dpt3-jri9.json?$where=arrest_date > '+"'"+start_datetime+"'"
+            container_name=' myfilesystem/Arrests/Arrests_Incremental'
+            file_name='arrests_'+str(date.today())+'.json'
 
-    elif end_point_name=='arrests_incr':
-        url='https://data.cityofchicago.org/resource/dpt3-jri9.json?$where=arrest_date > '+"'"+start_datetime+"'"
-        container_name=' myfilesystem/Arrests/Arrests_Incremental'
-        file_name='arrests_'+str(date.today())+'.json'
-
-    else :
-        return func.HttpResponse("please enter a vaild type_name")
+        else :
+            return func.HttpResponse("please enter a vaild type_name")
+    except Exception as E:
+        print(E)
 
     try:
         resp = requests.get(url)
         write_to_file(container_name,file_name,resp.content)
-    except:
-         return func.HttpResponse("Azure function failed due to API error or Blocb Connection error")
+    except Exception as E:
+        print(E)
+        return func.HttpResponse("Azure function failed due to API error or Blocb Connection error")
     else:
         return func.HttpResponse("File successfully loaded to Blob")
+
+    
 
         
 
