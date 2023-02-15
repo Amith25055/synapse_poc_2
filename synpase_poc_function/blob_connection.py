36bc45c65954f0f40f56ebc63b98636697f46f02
Amith25055 <104182312+Amith25055@users.noreply.github.com>
2023-02-15 15:43:42 +0530

    Update blob_connection.py

--- synpase_poc_function/blob_connection.py
+++ synpase_poc_function/blob_connection.py
@@ -1,7 +1,7 @@
 from azure.storage.blob import BlobClient
 
 
-def write_to_file(connection: str, container: str, filename: str, content: bytes):
+def write_to_file(container: str, filename: str, content: bytes):
     blob = BlobClient.from_connection_string(
         conn_str='DefaultEndpointsProtocol=https;AccountName=mydatalakestoragepoc;AccountKey=0tn3DNX5S9i44VlzCXL31JXmAcwat8u2futPxA+QNtPSdsYHxGkO1RxZeG+2qMNccOOEXRrbuRRb+AStEaAWRA==;EndpointSuffix=core.windows.net',
         container_name=container,
